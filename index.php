24dc4cb51d18ea5045fd5bfbcad8bc518661e79b
Emmanuel Castillo <emcastillo@csumb.edu>
2018-09-21 20:48:04 +0000

    added index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Silverjack Game</title>
+        <meta char="utf-8"/>
+    </head>
+    
+    <body>
+        
+    </body>
+    
+    <footer>
+        
+    </footer>
+</html>
